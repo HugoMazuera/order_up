59dd4f28c65895ef23562b90c7d17caa3eeb7d88
Hugo Mazuera <hmazuera89@outlook.com>
2022-07-13 17:15:21 -0600

    models

--- app/models.py
+++ app/models.py
@@ -1,5 +1,8 @@
+from turtle import back
 from flask_sqlalchemy import SQLAlchemy
 from flask_login import UserMixin
+from sqlalchemy import not_
+# from sqlalchemy import not_, null
 from werkzeug.security import generate_password_hash, check_password_hash
 
 
@@ -24,3 +27,68 @@ class Employee(db.Model, UserMixin):
 
     def check_password(self, password):
         return check_password_hash(self.password, password)
+
+class Menu(db.Model, UserMixin):
+
+    __tablename__ = "menus"
+
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(30), nullable=False)
+
+    menu_items = db.relationship("MenuItem", back_populates="menu")
+
+
+class MenuItem(db.Model, UserMixin):
+
+    __tablename__ = "menu_items"
+
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(50), nullable=False)
+    price = db.Column(db.Float, nullable=False)
+    menu_id = db.Column(db.Integer, db.ForeignKey("menus.id"), nullable=False)
+    menu_type_id = db.Column(db.Integer, db.ForeignKey("menu_item_types.id"), nullable=False)
+
+    menu = db.relationship("Menu", back_populates="menu_items")
+    type = db.relationship("MenuItemType", back_populates="item_types")
+
+class MenuItemType(db.Model, UserMixin):
+
+    __tablename__ = "menu_item_types"
+
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(20), nullable=False)
+
+    item_types = db.relationship("MenuItem", back_populates="type")
+
+class Table(db.Model, UserMixin):
+
+    __tablename__ = 'tables'
+
+    id = db.Column(db.Integer, primary_key=True)
+    number = db.Column(db.Integer, nullable=False, unique=True)
+    capacity = db.Column(db.Integer, nullable=False)
+
+    orders = db.relationship("Order", back_populates="table_order")
+
+class Order(db.Model, UserMixin):
+
+    __tablename__ = 'orders'
+
+    id = db.Column(db.Integer, primary_key=True)
+    employee_id = db.Column(db.Integer, db.ForeignKey("employees.id"),not_nullable=True )
+    table_id = db.Column(db.Integer, db.ForeignKey("tables.id"),not_nullable=True)
+    finished = db.Column(db.Boolean, not_nullable=True)
+
+    table_order = db.relationship("Table", back_populates="orders")
+    order_deets = db.relationship("OrderDetail", back_populates="one_order")
+
+
+class OrderDetail(db.Model, UserMixin):
+
+    __tablename__ = "order_details"
+
+    id = db.Column(db.Integer, primary_key=True)
+    order_id = db.Column(db.Integer, db.ForeignKey("orders.id"),not_nullable=True)
+    menu_item_id = db.Column(db.Integer, db.ForeignKey("menu_items.id"),not_nullable=True)
+
+    one_order = db.relationship("Orders", back_populates="order_deets")
--- database.py
+++ database.py
@@ -6,7 +6,7 @@ load_dotenv()
 # so that the environment variables are
 # properly loaded.
 from app import app, db
-from app.models import Employee
+from app.models import Employee, Menu, MenuItem, MenuItemType, Table
 
 
 with app.app_context():
@@ -14,5 +14,27 @@ with app.app_context():
     db.create_all()
 
     employee = Employee(name="Margot", employee_number=1234, password="password")
-    db.session.add(employee)
+
+    beverages = MenuItemType(name="Beverages")
+    entrees = MenuItemType(name="Entrees")
+    sides = MenuItemType(name="Sides")
+
+    dinner = Menu(name="Dinner")
+
+    fries = MenuItem(name="French fries", price=3.50, type=sides, menu=dinner)
+    drp = MenuItem(name="Dr. Pepper", price=1.0, type=beverages, menu=dinner)
+    jambalaya = MenuItem(name="Jambalaya", price=21.98, type=entrees, menu=dinner)
+
+
+    long_table = Table(number=1, capacity=8)
+    round_table = Table(number=2, capacity=6)
+
+    db.session.add(employee, dinner)
+    db.session.add(beverages, entrees)
+    db.session.add(fries, drp)
+    db.session.add(sides, jambalaya)
+    db.session.add(long_table)
+    db.session.add(round_table)
+
+
     db.session.commit()
